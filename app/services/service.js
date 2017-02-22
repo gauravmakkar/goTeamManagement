18e6b7466f9051cee56c40ab4978a162563cf46c
Gaurav MphRx <gmakkar119@gmail.com>
2017-02-22 14:02:12 +0530

    First Commit

--- app/services/service.js
+++ app/services/service.js
@@ -3,8 +3,8 @@
  */
 angular.module('myApp.service', []).factory("employee", function () {
 
-    var Employee = function (id,name, team, designation, table) {
-        this.id=id
+    var Employee = function (id, name, team, designation, table) {
+        this.id = id
         this.name = name
         this.team = team
         this.designation = designation
@@ -13,51 +13,92 @@ angular.module('myApp.service', []).factory("employee", function () {
     var employee_list = []
 
     var create_employees = function () {
-        employee_list.push(new Employee(1,"John", "A", "Software Engineer", {id:1,seat:1}))
-        employee_list.push(new Employee(2,"Bob", "A", "Sr. Software Engineer", {id:1,seat:2}))
-        employee_list.push(new Employee(3,"Ram", "B", "Test Engineer", {id:2,seat:3}))
-        employee_list.push(new Employee(4,"Mohan", "B", "Sr. Test Engineer", {id:2,seat:4}))
-        employee_list.push(new Employee(5,"Rock", "C", "Product Manager", {id:4,seat:3}))
+        employee_list.push(new Employee(1, "John", "A", "Software Engineer", {id: 1, seat: 1}))
+        employee_list.push(new Employee(2, "Bob", "A", "Sr. Software Engineer", {id: 1, seat: 2}))
+        employee_list.push(new Employee(3, "Ram", "B", "Test Engineer", {id: 2, seat: 3}))
+        employee_list.push(new Employee(4, "Mohan", "B", "Sr. Test Engineer", {id: 2, seat: 4}))
+        employee_list.push(new Employee(5, "Rock", "C", "Product Manager", {id: 4, seat: 3}))
     }
     create_employees()
-    var findEmployeeById=function(id){
-        var found_employee=employee_list.filter(function(employee){
-            return employee.id==id
+    var findEmployeeById = function (id) {
+        var found_employee = employee_list.filter(function (employee) {
+            return employee.id == id
         })
-        return found_employee.length>0?found_employee[0]:{}
+        return found_employee.length > 0 ? found_employee[0] : {}
     }
-    var findEmployeeByTable=function(table){
-        var found_employee=employee_list.filter(function(employee){
-            return employee.table.id==table.id && employee.table.seat==table.seat
+    var findEmployeeByTable = function (table) {
+        var found_employee = employee_list.filter(function (employee) {
+            return employee.table.id == table.id && employee.table.seat == table.seat
         })
-        return found_employee.length>0?found_employee[0]:{}
+        return found_employee.length > 0 ? found_employee[0] : {}
     }
-    var editEmployee=function(id,newEmployee){
-        var employee=findEmployeeById(id)
-        if(employee){
-            employee.name=newEmployee.name
-            employee.team=newEmployee.team
-            employee.designation=newEmployee.designation
-            employee.table=newEmployee.table
+    var editEmployee = function (id, newEmployee) {
+        var employee = findEmployeeById(id)
+        if (employee) {
+            employee.name = newEmployee.name
+            employee.team = newEmployee.team
+            employee.designation = newEmployee.designation
+            employee.table = newEmployee.table
         }
         return employee
     }
-   // editEmployee(2,{id:2,name:"Boby",team:"A",designation:"Sr. Software Engineer",table:2})
-    return {list: employee_list,findEmployeeByTable:findEmployeeByTable,editEmployee:editEmployee}
-
-}).filter("findEmployeeByTableSeat",function(employee){
-    return function(employeeList,table){
-      return  employee.findEmployeeByTable(table)
+    var list = function () {
+        return employee_list
     }
-}).directive("setEmployee",function(employee){
-    return {
-        restrict:"A",
-        link:function(scope,element,attrs){
-            scope.employee=employee.findEmployeeByTable({id:scope.table.id,seat:scope.seat})
-            scope.watch("employees",function(){
-                scope.employee=employee.findEmployeeByTable({id:scope.table.id,seat:scope.seat})
-            })
+    // editEmployee(2,{id:2,name:"Boby",team:"A",designation:"Sr. Software Engineer",table:2})
+    return {list: list, findEmployeeByTable: findEmployeeByTable, editEmployee: editEmployee}
 
+}).filter("findEmployeeByTableSeat", function (employee) {
+    return function (employeeList, table) {
+        return employee.findEmployeeByTable(table)
+    }
+}).directive("setEmployee", function (employee) {
+    return {
+        restrict: "A",
+        scope: true,
+        link: function (scope, element, attrs) {
+            scope.employee = employee.findEmployeeByTable({id: scope.table.id, seat: scope.seat})
+            if (scope.employee) {
+                element[0].addEventListener('dragstart', scope.handleDragStart, false);
+                element[0].addEventListener('dragend', scope.handleDragEnd, false);
+                element[0].addEventListener('drop', scope.handleDrop, false);
+                element[0].addEventListener('dragover', scope.handleDragOver, false);
+            }
+
+
+        },
+        controller: function ($scope) {
+            $scope.handleDragStart = function (e) {
+                if (!$scope.employee || !$scope.employee.id) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                } else {
+                    this.style.opacity = '0.4';
+                    e.dataTransfer.setData('json', JSON.stringify($scope.employee));
+                }
+
+            };
+
+            $scope.handleDragEnd = function (e) {
+                this.style.opacity = '1.0';
+            };
+
+            $scope.handleDrop = function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                var dataText = JSON.parse(e.dataTransfer.getData('json'));
+                dataText.table.id = $scope.table.id
+                dataText.table.seat = $scope.seat
+                $scope.employee = employee.editEmployee(dataText.id, dataText)
+                $scope.$apply()
+
+            };
+
+            $scope.handleDragOver = function (e) {
+                e.preventDefault(); // Necessary. Allows us to drop.
+                e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+                return false;
+            };
         }
     }
 })
--- app/table/dashboard.controller.js
+++ app/table/dashboard.controller.js
@@ -13,23 +13,13 @@ angular.module('myApp.dashboard.controller', ['ngRoute', 'myApp.service'])
     }])
 
     .controller('TableDashboardCtrl', ['$scope', 'employee', function ($scope, employee) {
-        $scope.employees = employee.list
+        $scope.employees={}
+        $scope.employees.list = employee.list()
 
         $scope.tables = [{id: 1, seats: [1, 2, 3, 4]}, {id: 2, seats: [1, 2, 3, 4]}, {
             id: 3,
             seats: [1, 2, 3, 4]
         }, {id: 4, seats: [1, 2, 3, 4]}, {id: 5, seats: [1, 2, 3, 4]}, {id: 6, seats: [1, 2, 3, 4]}]
 
-        $scope.editEmployeePosition = function (emply) {
-            employee.editEmployee(emply.id, {
-                name: emply.name + ' changed',
-                designation: emply.designation,
-                team: emply.team,
-                table: {id: 2, seat: 1}
-            })
-        }
     }])
-    .controller("EmployeeDetailsController", ['$scope', 'employee', function ($scope, employee) {
-        // console.log($scope.table)
-        $scope.employee = employee.findEmployeeByTable({id: $scope.table.id, seat: $scope.seat})
-    }]);
+    
